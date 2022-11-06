bac3055c3f669ec2e47b68d4a3179e2c98c4d038
Alexey <alex616@bk.ru>
2022-11-06 20:03:07 +0300

    старт

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Аня")
+{
+    Console.WriteLine("Ура! Это же АНЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
